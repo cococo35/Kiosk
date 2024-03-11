f1ee58582eeaf2169a429a4f81eba3f3c4e3e5bd
cococo35 <taejun0513@gmail.com>
2024-03-11 16:47:13 +0900

    Lv2/Kiosk 기능 구현

---
+++ app/src/main/java/com/android/kiosk/KioskClass.kt
@@ -0,0 +1,60 @@
+package com.android.kiosk
+
+ class KioskClass {
+
+    fun MainMenu() {
+        println("[Star Cafe Menu]")
+        println("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ")
+        println("[1] 콜드브루 커피")
+        println("[2] 블론드")
+        println("[3] 에스프레소")
+        println("[4] 프라푸치노")
+        println("[5] 케이크")
+        println("[0] 키오스크 종료")
+    }
+
+    fun ColdBrewMenu() {
+        println("[ColdBrew Menu]")
+        println("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ")
+        println("[1] 시그니처 더 블랙 콜드 브루 | W 9,600 | ")
+        println("[2] 돌체 콜드 브루           | W 6,000 |")
+        println("[3] 바닐라 크림 콜드 브루     | W 5,800 |")
+        println("[0] 메인 메뉴로")
+    }
+
+     fun BlondMenu() {
+         println("[Blond Menu]")
+         println("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ")
+         println("[1] 블론드 바닐라 더블샷 마키아또 | W 5,900 |")
+         println("[2] 블론드 카페라떼             | W 5,000 |")
+         println("[3] 블론드 카페 아메리카노       | W 4,500 |")
+         println("[0] 메인 메뉴로")
+     }
+
+     fun EspressoMenu() {
+         println("[Espresso Menu]")
+         println("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ")
+         println("[1] 카페 아메리카노 | W 4,500 |")
+         println("[2] 카푸치노       | W 5,000 |")
+         println("[3] 에스프레소     | W 4,500 |")
+         println("[0] 메인 메뉴로")
+     }
+
+     fun FrappuccinoMenu() {
+         println("[Frappuccino Menu]")
+         println("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ")
+         println("[1] 자바 칩 프라푸치노    | W 6,300 |")
+         println("[2] 카라멜 프라푸치노     | W 5,900 |")
+         println("[3] 에스프레소 프라푸치노  | W 5,500 |")
+         println("[0] 메인 메뉴로")
+     }
+
+     fun CakeMenu() {
+         println("[Cake Menu]")
+         println("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ")
+         println("[1] 체리 포레누아 케이크       | W 7,500 |")
+         println("[2] 마스카포네 티라미슈 케이크  | W 5,900 |")
+         println("[3] 클라우드 치즈 케이크       | W 5,500 |")
+         println("[0] 메인 메뉴로")
+     }
+}
--- app/src/main/java/com/android/kiosk/KioskMain.kt
+++ app/src/main/java/com/android/kiosk/KioskMain.kt
@@ -1,15 +1,10 @@
 package com.android.kiosk
 
 fun main() {
+    var Work = KioskClass()
     while(true) {
-        println("[Star Cafe Menu]")
-        println("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ")
-        println("[1] 콜드브루 커피")
-        println("[2] 블론드")
-        println("[3] 에스프레소")
-        println("[4] 프라푸치노")
-        println("[5] 케이크")
-        println("[0] 키오스크 종료")
+
+        Work.MainMenu()
 
         var sign: Int = readLine()!!.toInt()
 
@@ -20,60 +15,35 @@ fun main() {
             }
 
             1 -> {
-                println("[ColdBrew Menu]")
-                println("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ")
-                println("[1] 시그니처 더 블랙 콜드 브루")
-                println("[2] 돌체 콜드 브루")
-                println("[3] 바닐라 크림 콜드 브루")
-                println("[0] 메인 메뉴로")
+                Work.ColdBrewMenu()
 
                 sign = readLine()!!.toInt()
                 if (sign == 0) continue
             }
 
             2 -> {
-                println("[Blond Menu]")
-                println("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ")
-                println("[1] 블론드 바닐라 더블샷 마키아또")
-                println("[2] 블론드 카페라떼")
-                println("[3] 블론드 카페 아메리카노")
-                println("[0] 메인 메뉴로")
+                Work.BlondMenu()
 
                 sign = readLine()!!.toInt()
                 if (sign == 0) continue
             }
 
             3 -> {
-                println("[Espresso Menu]")
-                println("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ")
-                println("[1] 카페 아메리카노")
-                println("[2] 카푸치노")
-                println("[3] 에스프레소")
-                println("[0] 메인 메뉴로")
+                Work.EspressoMenu()
 
                 sign = readLine()!!.toInt()
                 if (sign == 0) continue
             }
 
             4 -> {
-                println("[Frappuccino Menu]")
-                println("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ")
-                println("[1] 자바 칩 프라푸치노")
-                println("[2] 카라멜 프라푸치노")
-                println("[3] 에스프레소 프라푸치노")
-                println("[0] 메인 메뉴로")
+                Work.FrappuccinoMenu()
 
                 sign = readLine()!!.toInt()
                 if (sign == 0) continue
             }
 
             5 -> {
-                println("[Cake Menu]")
-                println("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ")
-                println("[1] 체리 포레누아 케이크")
-                println("[2] 마스카포네 티라미슈 케이크")
-                println("[3] 클라우드 치즈 케이크")
-                println("[0] 메인 메뉴로")
+                Work.CakeMenu()
 
                 sign = readLine()!!.toInt()
                 if (sign == 0) continue
