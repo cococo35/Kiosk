0120707446caa814c7f1cefe12eb3c696dad3b04
cococo35 <taejun0513@gmail.com>
2024-03-12 21:07:20 +0900

    중간 저장

--- app/src/main/java/com/android/kiosk/KioskMain.kt
+++ app/src/main/java/com/android/kiosk/KioskMain.kt
@@ -1,5 +1,7 @@
 package com.android.kiosk
 
+import java.lang.NumberFormatException
+
 fun main() {
     lateinit var work : KioskClass
     val coldBrewList = mutableListOf<String>()
@@ -19,93 +21,109 @@ fun main() {
         println("[5] 케이크")
         println("[0] 키오스크 종료")
 
-        var select: Int = readln().toInt()
+        try {
+            var select: Int = readln().toInt()
 
-        when (select) {
-            0 -> {
-                println("키오스크 종료")
-                return
-            }
+            if(select in 0..5 || select == 9) {
 
-            1 -> {
-                work = KioskClass(ColdBrew())
-                work.menu(coldBrewList)
+                when (select) {
+                    0 -> {
+                        println("키오스크 종료")
+                        return
+                    }
 
-                select = readln().toInt()
-                if(select == 0) continue
-            }
-
-            2 -> {
-                work = KioskClass(Blond())
-                work.menu(blondList)
-
-                select = readln().toInt()
-                if(select == 0) continue
-            }
-
-            3 -> {
-                work = KioskClass(Espresso())
-                work.menu(espressoList)
-
-                select = readln().toInt()
-                if(select == 0) continue
-            }
-
-            4 -> {
-                work = KioskClass(Frappuccino())
-                work.menu(frappuccinoList)
-
-                select = readln().toInt()
-                if(select == 0) continue
-            }
-
-            5 -> {
-                work = KioskClass(Cake())
-                work.menu(cakeList)
-
-                select = readln().toInt()
-                if(select == 0) continue
-            }
-
-            9 -> {
-                println("추가할 메뉴를 선택해 주세요")
-                println("[1] 콜드브루 커피, [2] 블론드, [3] 에스프레소, [4] 프라푸치노, [5] 케이크")
-                select = readln().toInt()
-
-                when(select) {
                     1 -> {
                         work = KioskClass(ColdBrew())
-                        println("메뉴의 이름과 가격을 적어주세요! ([메뉴이름] W [가격])")
-                        work.init(readln(), coldBrewList)
+                        work.menu(coldBrewList)
+
+                        select = readln().toInt()
+                        if (select == 0) continue
                     }
+
                     2 -> {
                         work = KioskClass(Blond())
-                        println("메뉴의 이름과 가격을 적어주세요! ([메뉴이름] W [가격])")
-                        work.init(readln(), blondList)
+                        work.menu(blondList)
+
+                        select = readln().toInt()
+                        if (select == 0) continue
                     }
+
                     3 -> {
                         work = KioskClass(Espresso())
-                        println("메뉴의 이름과 가격을 적어주세요! ([메뉴이름] W [가격])")
-                        work.init(readln(), espressoList)
+                        work.menu(espressoList)
+
+                        select = readln().toInt()
+                        if (select == 0) continue
                     }
+
                     4 -> {
                         work = KioskClass(Frappuccino())
-                        println("메뉴의 이름과 가격을 적어주세요! ([메뉴이름] W [가격])")
-                        work.init(readln(), frappuccinoList)
+                        work.menu(frappuccinoList)
+
+                        select = readln().toInt()
+                        if (select == 0) continue
                     }
+
                     5 -> {
                         work = KioskClass(Cake())
-                        println("메뉴의 이름과 가격을 적어주세요! ([메뉴이름] W [가격])")
-                        work.init(readln(), cakeList)
+                        work.menu(cakeList)
+
+                        select = readln().toInt()
+                        if (select == 0) continue
+                    }
+
+                    9 -> {
+                        println("추가할 메뉴를 선택해 주세요")
+                        println("[1] 콜드브루 커피, [2] 블론드, [3] 에스프레소, [4] 프라푸치노, [5] 케이크")
+                        try {
+                            select = readln().toInt()
+
+                            if (select in 1..5) {
+
+                                when (select) {
+                                    1 -> {
+                                        work = KioskClass(ColdBrew())
+                                        println("메뉴의 이름과 가격을 적어주세요! ([메뉴이름] W [가격])")   // 메뉴 이름 양식 try..catch 만들기
+                                        work.init(readln(), coldBrewList)
+                                    }
+
+                                    2 -> {
+                                            work = KioskClass(Blond())
+                                            println("메뉴의 이름과 가격을 적어주세요! ([메뉴이름] W [가격])")   // 메뉴 이름 양식 try..catch 만들기
+                                            work.init(readln(), blondList)
+                                        }
+
+                                    3 -> {
+                                            work = KioskClass(Espresso())
+                                            println("메뉴의 이름과 가격을 적어주세요! ([메뉴이름] W [가격])")   // 메뉴 이름 양식 try..catch 만들기
+                                            work.init(readln(), espressoList)
+                                        }
+
+                                    4 -> {
+                                            work = KioskClass(Frappuccino())
+                                            println("메뉴의 이름과 가격을 적어주세요! ([메뉴이름] W [가격])")   // 메뉴 이름 양식 try..catch 만들기
+                                            work.init(readln(), frappuccinoList)
+                                        }
+
+                                    5 -> {
+                                            work = KioskClass(Cake())
+                                            println("메뉴의 이름과 가격을 적어주세요! ([메뉴이름] W [가격])")   // 메뉴 이름 양식 try..catch 만들기
+                                            work.init(readln(), cakeList)
+                                        }
+                                }
+                            } else {
+                                println("올바른 숫자를 입력해주세요! 메인 메뉴로 돌아갑니다...")
+                                println("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ")
+                            }
+                        } catch (e: NumberFormatException) {
+                            println("숫자를 입력해주세요! 메인 메뉴로 돌아갑니다...")
+                            println("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ")
+                        }
                     }
                 }
-            }
+            } else println("올바른 숫자를 입력해주세요!")
+        } catch (e:NumberFormatException) {
+            println("숫자를 입력해주세요!")
         }
     }
 }
-
-//- 메뉴판 번호는 숫자기에 숫자를 입력해야 하는데 문자를 입력했을 때,
-//메뉴판에 없는 숫자를 입력했을 때,
-//다시 입력할 수 있도록 **`예외처리`** 합니다.
-//- 현재 잔액과 가격을 비교해서 구매 가능한 상태를 클래스에 추가적으로 구현합니다.
-//- 예를 들어 구매할만큼 충분한 돈이 있는지도 상태에 포함시킵니다.
